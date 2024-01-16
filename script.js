f719eaf6089289bd13eca71f71e66ea8e9b93e87
aaliya313 <aaliyaladha@hotmail.co.uk>
2024-01-16 17:17:31 +0000

    Added js logic

--- script.js
+++ script.js
@@ -1,48 +1,69 @@
-//Objects of userInput 
-
+//Object of user Input values
 const userInputValues = {
-    Alcohols: 'Alcohols',
     Aldehydes: 'Aldehydes',
-    Ketones: 'Ketones'
+    Ketones: 'Ketones',
+    Benzenes: 'Benzene'
     
 };
 
 
-//For loop to iterate through, to randomly generate
-const organicMechanismsValues  = {
+//Arrays of Organic Mechanisms
+const benzenelMechanismsValues  = [
+    'Nitration',
+    'Chlorination',
+    'Friedel-Crafts'
+];
 
+const aldehydeMechanismsValues  = [
+    'Enamine Formation',
+    'Oxidation'
+];
 
-
-
-
-
-};
-
+const ketoneMechanismsValues = [
+    'Acetal Formation',
+    'Imine Formation',
+    'Oxidation'
+];
 
 
 
 function generateOrganicMechanisms(userInput) {
-    let userInput = document.getElementById('input').getText();
-    
-    
 
+    alert('button clicked');
+    let userInput = document.getElementById('input').getText();
+
+    let randomBenzeneMechanism = benzenelMechanismsValues[Math.floor(Math.random()*benzenelMechanismsValues.length)];
+
+    let randomAldehydeMechanism = aldehydeMechanismsValues[Math.floor(Math.random()*aldehydeMechanismsValues.length)];
+
+    let randomKetoneMechanism = ketoneMechanismsValues[Math.floor(Math.random()*ketoneMechanismsValues.length)];
     
     if (userInput === null) {
         alert("Please enter a topic you would like to study")
 
     } 
-    else if (userInput === userInputValues.Alcohols) {
 
+    else if (userInput === userInputValues.Benzenes) {
+        for (let i = 0; i < benzenelMechanismsValues.length; i++ ) {
+            alert(randomBenzeneMechanism);
+        }
     }
+
     else if (userInput === userInput.Aldehydes) {
+        for (let i = 0; i < aldehydeMechanismsValues.length; i++ ) {
+            alert(randomAldehydeMechanism);
+        }
 
     }
     else if (userInput === userInput.Ketones) {
+        for (let i = 0; i < ketoneMechanismsValues.length; i++ ) {
+            alert(randomKetoneMechanism);
+        }
 
     }
-    
-
-
+    else {
+        alert("Not a recognised topic, please type in either Benzene, Aldehydes, Ketones")
+    }
 
 };
 
@@ -56,5 +77,10 @@ function buttonClickEvent() {
         });
 
 };
+
+buttonClickEvent();
+generateOrganicMechanisms();
+
+
 
 
